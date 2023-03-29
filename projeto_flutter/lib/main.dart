c8a7e335deb598eba14e740718bb8637e804b33b
CAMILA ALVES FERREIRA <camila.aferreira24@senacsp.edu.br>
2023-03-29 20:27:35 -0300

    wip 2

--- projeto_flutter/lib/main.dart
+++ projeto_flutter/lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/': (_) => const HomePage(),
         '/splash': (_) => const SplashView(),
--- projeto_flutter/lib/views/login.dart
+++ projeto_flutter/lib/views/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:validadores/validadores.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,44 +11,90 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(28),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'images/icon.png',
-              width: 120,
-              height: 120,
-            ),
-            const Text('Login', textAlign: TextAlign.start,),
-            const TextField(
-              decoration: InputDecoration(
-                label: Text('Login'),
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 28),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 120),
+                child: Image.asset(
+                  'images/icon.png',
+                  width: 98,
+                  height: 98,
+                ),
               ),
-            ),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                label: Text('Senha'),
+              const Text(
+                'Login',
+                textAlign: TextAlign.start,
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 32),
-              child: ElevatedButton(
-                onPressed: null,
-                child: Text('Entrar'),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "*campo obrigatório";
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: const InputDecoration(
+                  label: Text('Email'),
+                ),
               ),
-            ),
-            const ElevatedButton(
-              onPressed: null,
-              child: Text(
-                'Cadastre-se',
+              TextFormField(
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "*campo obrigatório";
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: const InputDecoration(
+                  label: Text('Senha'),
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 32),
+                child: GestureDetector(
+                  child: Container(
+                    height: 30,
+                    width: 120,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 11, 203, 176),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Entrar',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    if (formKey.currentState!.validate()) {
+                      print('Valido');
+                    }
+                  },
+                ),
+              ),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  //TODO: Ir para a tela de cadastro
+                },
+                child: const Text(
+                  'Cadastre-se',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

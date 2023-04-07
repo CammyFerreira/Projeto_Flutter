237f10d1a98b4035c7266154d4f0d3d3f8c46474
camila alves <Camila>
2023-04-07 17:58:57 -0300

    login integrado

--- projeto_flutter/lib/controllers/login_controller.dart
+++ projeto_flutter/lib/controllers/login_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
 class LoginController {
@@ -14,22 +17,35 @@ class LoginController {
     var client = http.Client();
     try {
       isLoading.value = true;
-      final url = Uri.parse('https://minhasapis.com.br/login/');
+      final url = Uri.parse('http://10.0.2.2:8000/api/login');
       final resposta = await client.post(
         url,
         body: {
           'email': _login,
           'senha': _pass,
         },
+      ).timeout(
+        const Duration(seconds: 10),
       );
       if (resposta.statusCode == 200) {
         return true;
       } else {
         return false;
       }
+    } on TimeoutException {
+      Fluttertoast.showToast(
+        msg: 'A conexão expirou. Por favor, tente novamente mais tarde.',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+      return false;
     } finally {
       client.close();
-      isLoading.value = true;
+      isLoading.value = false;
     }
   }
 }

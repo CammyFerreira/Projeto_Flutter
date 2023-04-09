40e0dd8d5f0a70ca64a7130009f5a17d8299a65a
camila alves <Camila>
2023-04-09 19:41:28 -0300

    lista de produtos integrada

---
+++ projeto_flutter/lib/views/pdp.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+// import 'package:projeto_flutter/helpers/helpers.dart';
+import 'package:projeto_flutter/models/product.dart';
+
+class PDPView extends StatelessWidget {
+  final Product product;
+
+  const PDPView({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(product.nome),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              color: Colors.white,
+              Icons.shopping_cart,
+            ),
+            onPressed: () {
+              //TODO navegar para o carrinho
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+            ),
+            // child: Helpers.getProductImage(
+            //   '',
+            //   310,
+            //   double.infinity,
+            // ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      product.nome,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      "R\$ ${double.parse(product.preco).toStringAsFixed(2)}}",
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  product.descricao,
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Spacer(),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size(double.infinity, 50),
+              fixedSize: const Size(200, 50),
+            ),
+            child: const Text("Adicionar ao carrinho"),
+          )
+        ],
+      ),
+    );
+  }
+}

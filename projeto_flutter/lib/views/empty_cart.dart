92293dc3a52a2e4b7d4b377c0885ebeca2c9931e
camila alves <Camila>
2023-05-11 15:31:34 -0300

    carrinho vazio

---
+++ projeto_flutter/lib/views/empty_cart.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class EmptyCart extends StatelessWidget {
+  const EmptyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                top: 48.0,
+                bottom: 32,
+              ),
+              child: Text(
+                "Seu carrinho está vazio",
+                style: TextStyle(
+                  fontSize: 32,
+                ),
+              ),
+            ),
+            SizedBox(
+              child: Image.asset(
+                'images/cart.png',
+                height: 250,
+                width: 250,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

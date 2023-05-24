984dac007d77771e589fed40fc56d591afa6e465
DANTONI PAIVA SILVA <dantoni.psilva@senacsp.edu.br>
2023-05-24 20:07:49 -0300

    Lista de categorias antes API

--- projeto_flutter/lib/main.dart
+++ projeto_flutter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_flutter/views/carrinho.dart';
 import 'package:projeto_flutter/views/bottom_navigation.dart';
+import 'package:projeto_flutter/views/list_categories.dart';
 import 'package:projeto_flutter/views/list_page.dart';
 import 'package:projeto_flutter/views/pedido_finalizado.dart';
 import 'package:projeto_flutter/views/splash.dart';
@@ -38,7 +39,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: primaryColor,
       ),
-      initialRoute: '/',
+      initialRoute: '/categorias',
       routes: {
         '/': (_) => const HomePage(),
         '/splash': (_) => const SplashView(),
@@ -46,6 +47,7 @@ class MyApp extends StatelessWidget {
         '/list': (_) => const ListPage(),
         '/carrinho': (_) => const CarrinhoView(),
         '/finalizado': (_) => const PedidoFinalizado(),
+        '/categorias': (_) => const ListCategories(),
       },
     );
   }

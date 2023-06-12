e2b2874ed7979b16c1f00176206dd8937f0b6df1
unknown <robson_herculano@hotmail.com>
2023-06-12 17:25:46 -0300

    Finalizando a integração do cadastro

--- projeto_flutter/lib/cadastro.dart
+++ projeto_flutter/lib/cadastro.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:brasil_fields/brasil_fields.dart';
 import 'package:projeto_flutter/controllers/cadastro_controller.dart';
-
-import '../controllers/cadastro_controller.dart';
 
 class CadastroView extends StatelessWidget {
   final CadastroController _controller = CadastroController();
@@ -16,117 +12,130 @@ class CadastroView extends StatelessWidget {
   Widget build(BuildContext context) {
     final formKey = GlobalKey<FormState>();
     return Scaffold(
-        body: Form(
-      key: formKey,
-      child: Container(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset(
-                'images/icon.png',
-                height: 90,
-                width: 90,
-              ),
-              const Text('Cadastre-se'),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Campo obrigatório";
-                  }
-                },
-                decoration: const InputDecoration(
-                  label: Text('Nome'),
-                ),
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Campo obrigatório";
-                  }
-                },
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  // obrigatório
-                  FilteringTextInputFormatter.digitsOnly,
-                  CpfInputFormatter(),
-                ],
-                decoration: const InputDecoration(
-                  label: Text('CPF'),
-                ),
-              ),
-              TextFormField(
-                onChanged: _controller.setCadastro,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Campo obrigatório";
-                  }
-                  if (!EmailValidator.validate(value, true)) {
-                    return 'Email inválido';
-                  }
-                },
-                decoration: const InputDecoration(
-                  label: Text('Email'),
-                ),
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Campo obrigatório";
-                  }
-                },
-                decoration: const InputDecoration(
-                  label: Text('Senha'),
-                ),
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Campo obrigatório";
-                  }
-                },
-                decoration: const InputDecoration(
-                  label: Text('Endereço'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 32,
-                ),
-                child: ValueListenableBuilder<bool>(
-                  valueListenable: _controller.isLoading,
-                  builder: (_, isLoading, __) => isLoading
-                      ? const CircularProgressIndicator()
-                      : ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 11, 203, 176),
-                            foregroundColor: Colors.white,
-                          ),
-                          onPressed: () {
-                            if (formKey.currentState!.validate()) {
-                              _controller.cadastrar().then((result) {
-                                if (result) {
-                                  Navigator.of(context)
-                                      .pushReplacementNamed('/');
-                                } else {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                      content: Text('Email ou senha inválidos'),
-                                      duration: Duration(seconds: 5),
-                                    ),
+        body: SingleChildScrollView(
+      child: Form(
+        key: formKey,
+        child: Container(
+            padding: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'images/icon.png',
+                    height: 90,
+                    width: 90,
+                  ),
+                  const Text('Cadastre-se'),
+                  TextFormField(
+                    onChanged: _controller.setNome,
+                    maxLength: 100,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Campo obrigatório";
+                      }
+                      if (value.length < 3) {
+                        return "O nome deve conter no mínimo 3 caracteres";
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      label: Text('Nome'),
+                    ),
+                  ),
+                  TextFormField(
+                    onChanged: _controller.setCpf,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Campo obrigatório";
+                      }
+                      if (value.length < 11) {
+                        return "O cpf deve ter pelo menos 11 caracteres";
+                      }
+                    },
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      CpfInputFormatter(),
+                    ],
+                    decoration: const InputDecoration(
+                      label: Text('CPF'),
+                    ),
+                  ),
+                  TextFormField(
+                    onChanged: _controller.setEmail,
+                    maxLength: 100,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Campo obrigatório";
+                      }
+                      if (!EmailValidator.validate(value, true)) {
+                        return 'Email inválido';
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      label: Text('Email'),
+                    ),
+                  ),
+                  TextFormField(
+                    maxLength: 100,
+                    onChanged: _controller.setPass,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Campo obrigatório";
+                      }
+                      if (value.length < 8) {
+                        return "A senha deve conter pelo menos 8 caracteres";
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      label: Text('Senha'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 32,
+                    ),
+                    child: ValueListenableBuilder<bool>(
+                      valueListenable: _controller.isLoading,
+                      builder: (_, isLoading, __) => isLoading
+                          ? const CircularProgressIndicator()
+                          : ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    const Color.fromARGB(255, 11, 203, 176),
+                                foregroundColor: Colors.white,
+                              ),
+                              onPressed: () async {
+                                if (formKey.currentState!.validate()) {
+                                  await _controller.cadastrar().then(
+                                    (result) {
+                                      if (result) {
+                                        Navigator.of(context)
+                                            .pushReplacementNamed('/');
+                                      } else {
+                                        isLoading = false;
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          const SnackBar(
+                                            content: Text('Campos inválidos'),
+                                            duration: Duration(seconds: 5),
+                                          ),
+                                        );
+                                      }
+                                    },
                                   );
                                 }
-                              });
-                            }
-                          },
-                          child: const Text('Cadastrar'),
-                        ),
-                ),
+                              },
+                              child: const Text('Cadastrar'),
+                            ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          )),
+            )),
+      ),
     ));
   }
 }
--- projeto_flutter/lib/controllers/cadastro_controller.dart
+++ projeto_flutter/lib/controllers/cadastro_controller.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CadastroController {
-
   ValueNotifier<bool> isLoading = ValueNotifier<bool>(false);
 
-  String? _cadastro;
-  setCadastro(String value) => _cadastro = value;
+  String? _email;
+  setEmail(String value) => _email = value;
 
   String? _pass;
   setPass(String value) => _pass = value;
 
+  String? _nome;
+  setNome(String value) => _nome = value;
+
+  String? _cpf;
+  setCpf(String value) => _cpf = value;
+
+  late String? cpfSemFormatacao;
+
+  String? formatarCpf() {
+    if (_cpf != null) {
+      cpfSemFormatacao = _cpf!.replaceAll(RegExp(r'[^\d]'), '');
+      return cpfSemFormatacao;
+    }
+    return null;
+  }
+
   Future<bool> cadastrar() async {
-    isLoading.value = true;
-    await Future.delayed(const Duration(seconds: 2));
-    isLoading.value = false;
-    return _cadastro == 'adm@adm' && _pass == '123';
+    var client = http.Client();
+    try {
+      isLoading.value = true;
+      final url = Uri.parse('http://10.0.2.2:8000/api/cadastro');
+      final resposta = await client.post(
+        url,
+        body: {
+          'USUARIO_NOME': _nome,
+          'USUARIO_EMAIL': _email,
+          'USUARIO_SENHA': _pass,
+          'USUARIO_CPF': formatarCpf(),
+        },
+      );
+      if (resposta.statusCode == 200) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 }
